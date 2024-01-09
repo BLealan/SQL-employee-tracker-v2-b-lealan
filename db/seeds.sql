f428959c33d4253d54bf8873e854a1e556589082
BLealan <143086275+BLealan@users.noreply.github.com>
2024-01-09 16:27:01 +0000

    added seed data

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO department (department_name)
+VALUES  ("IT"),
+        ("Accounting"),
+        ("Management"),
+        ("Human Resources");
+
+INSERT INTO role (title, salary)
+VALUES  ("Line Manager", 35000),
+        ("CEO", 120000),
+        ("Accountant", 30000),
+        ("Cyber Security Expert", 28000);
+
+INSERT INTO employee (first_name, last_name)
+VALUES  ("Aaron", "Aaronson"),
+        ("Zach", "Zukowski");
